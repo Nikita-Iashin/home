aebec821e9e7a0ea5bdf09b9192a8977f4b51e1a
Nikita_Iashin <nikita_iashin@epam.com>
2022-07-21 08:38:36 +0400

    a start of taf for 2022 TA training

---
+++ ta2022/src/main/java/com/epam/ta2022/Ta2022Application.java
@@ -0,0 +1,13 @@
+package com.epam.ta2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta2022Application.class, args);
+	}
+
+}
